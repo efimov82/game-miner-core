50c126be30c0e9e51ef472f0991cedce1701d7c9
Dan Efimov <daniil.efimov@specific-group.com>
2022-01-19 21:27:58 +0300

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const cors = {
+    origin: ['*'],
+  };
+
+  const app = await NestFactory.create(AppModule, { cors });
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
